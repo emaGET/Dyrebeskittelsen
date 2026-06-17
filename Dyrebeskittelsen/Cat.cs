06086b270d89f899b2d8f2167cde3864b4067d2f
emaGET <emanuelepini@getacademy.no>
2026-06-17 15:37:13 +0200

    let's start from the beginning

---
+++ Dyrebeskittelsen/Cat.cs
@@ -0,0 +1,8 @@
+﻿namespace Dyrebeskittelsen;
+public class Cat
+{
+    private string _name;
+    private string _breed;
+    private int _age;
+    private bool _availableForAdoption;
+}
---
+++ Dyrebeskittelsen/Program.cs
@@ -0,0 +1,7 @@
+﻿using Dyrebeskittelsen;
+
+Cat c1 = new Cat();
+c1._name = "nuka";
+c1._breed = "Tuxedo";
+c1._age = 2;
+c1._availableForAdoption = true;
