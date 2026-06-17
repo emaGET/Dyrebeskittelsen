0a94e8773e2b273312d4a4cb4d9a8b514afca6de
emaGET <emanuelepini@getacademy.no>
2026-06-17 16:30:48 +0200

    continue

--- Dyrebeskittelsen/Cat.cs
+++ Dyrebeskittelsen/Cat.cs
@@ -1,8 +1,21 @@
 ﻿namespace Dyrebeskittelsen;
 public class Cat
 {
-    private string _name;
-    private string _breed;
-    private int _age;
-    private bool _availableForAdoption;
+    public string Name { get; private set; }
+    public string Breed { get; private set; }
+    public int Age { get; private set; }
+    public bool AvailableForAdoption { get; private set; }
+
+    public Nuka(string name)
+    {
+        Name = name;
+    }
+
+    public Miso(string name, string breed, int age, bool adoptable)
+    {
+        Name = name;
+        Breed = breed;
+        Age = age;
+        AvailableForAdoption = adoptable;
+    }
 }
--- Dyrebeskittelsen/Program.cs
+++ Dyrebeskittelsen/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Dyrebeskittelsen;
 
-Cat c1 = new Cat();
-c1._name = "nuka";
-c1._breed = "Tuxedo";
-c1._age = 2;
-c1._availableForAdoption = true;
+Cat c1 = new Cat("Nuka", "Tuxedo", 2, true);
+Cat c2 = new Cat("Miso", "Orange", 2, true);
+Console.WriteLine(c1.Name);
+Console.WriteLine(c2.Name);
